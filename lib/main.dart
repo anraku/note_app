9ce9d582e6855c47841dfc6fc83e6825607a78e8
anraku <anraku@daimorinoMacBook.local>
2018-06-08 07:43:36 +0900

    [update] fix some words

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Note App',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new Home(title: 'Flutter Demo Home Page'),
+      home: new Home(title: 'Note List'),
     );
   }
 }
